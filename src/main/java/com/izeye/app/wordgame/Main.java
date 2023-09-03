66143f5697c5addd0ee7876a7e9f1b91c0fc89cf
Johnny Lim <izeye@naver.com>
2023-09-03 16:43:29 +0900

    Apply Spring Java Format

---
+++ src/main/java/com/izeye/app/wordgame/Main.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2012-2023 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.izeye.app.wordgame;
+
+import java.util.Scanner;
+
+/**
+ * Main class.
+ *
+ * @author Johnny Lim
+ */
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("사과");
+		String line;
+		while ((line = new Scanner(System.in).nextLine()) != null) {
+			if (line.equals("apple")) {
+				break;
+			}
+			System.out.println("Try again!");
+		}
+		System.out.println("Congratulation!");
+	}
+
+}
