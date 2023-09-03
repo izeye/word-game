4ce932e5ef1d696ea8234b46c84329507962fbd3
Johnny Lim <izeye@naver.com>
2023-09-03 21:39:32 +0900

    Fix sort_words.sh

--- scripts/sort_words.sh
+++ scripts/sort_words.sh
@@ -1,1 +1,1 @@
-sort src/main/resources/english_to_korean.csv -o src/main/resources/english_to_korean.csv
+sort src/main/resources/words/english_to_korean.csv -o src/main/resources/words/english_to_korean.csv
